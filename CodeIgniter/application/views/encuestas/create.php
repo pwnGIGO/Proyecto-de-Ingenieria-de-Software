ff79cc2cfcb5f6d17e7ae76bce84fe00781d8a90
fersanti2896 <fersanti2896@gmail.com>
2019-07-13 03:54:56 -0500

    Retrabajo Fernando: Se agregó límite de caracteres a cada inputText, además de mensajes en su placeholder, y se modificaron algunos vistas (index) para la mejor estructuración de los botones

--- CodeIgniter/application/views/encuestas/create.php
+++ CodeIgniter/application/views/encuestas/create.php
@@ -1,13 +1,12 @@
-<div align="center"><h1>Crea una encuesta</h1></div>
-<br><br>
-		
+	<div align="center"><h1>Crea una encuesta</h1></div>
+	<br>	
 		<?php
 			$fecha_actual=date("Y-m-d");
 			$crearEncuesta_url = base_url()."Encuestas/create";
 			// Atributos de la caja de texto
 			$encuestas= array(
 				'name' => 'nombre',
-				'placeholder' => 'Nombre',
+				'placeholder' => ' Nombre de la Encuesta',
 				'required' => 'required',
 				'size' => '60',
 				'maxlength' => '50',
@@ -38,15 +37,12 @@
 
 			$numero_encuestas= array(
 				'name' => 'numero_encuestas',
-				'placeholder' => '¿ cuantas ?',
+				'placeholder' => '#',
 				'type' => 'number',
 				'required' => 'required',
 				'min' => '1',
-				'max' => '50',
-					
+				'max' => '50'
 			);
-
-			
 			
 		?>
 
@@ -56,25 +52,24 @@
 					
 						<div style="margin:25px; text-align: left;">
 							<i class="fa fa-envelope prefix white-text"></i>
-							<?= form_label('Nombre de la encuesta') ?>
+							<?= form_label('Nombre de la Encuesta:') ?><br>
 							<?= form_input($encuestas) ?><br><br>
 
-							<?= form_label('Fecha inicio') ?>
+							<?= form_label('Fecha Inicio: ') ?><br>
 							<?= form_input($fecha_i) ?><br><br>
 							
-							<?= form_label('Fecha Fin') ?>
+							<?= form_label('Fecha Fin: ') ?><br>
 							<?= form_input($fecha_f) ?><br><br>
 							
-							<?= form_label('Numero de encuestas') ?>
+							<?= form_label('Numero de Encuestas: ') ?>
 							<?= form_input($numero_encuestas) ?><br><br>
 
-							<?= form_label('Elige un cuestionario') ?>
+							<?= form_label('Elige un Cuestionario:') ?>
 							<select name="cuestionario_elegido">				
 								<?php 
 									if($cuestionarios){		
 										foreach ($cuestionarios->result() as $cuestionario) {?>
-											<option value="<?=$cuestionario->idCuestionario ?>"><?= $cuestionario->nombreCuestionario ?></option>
-											   
+											<option value="<?=$cuestionario->idCuestionario ?>"><?= $cuestionario->nombreCuestionario ?></option>   
 									<?php } 
 									} 
 								?>
@@ -83,13 +78,10 @@
 						</div>
 
 						<div align="center">
-							<input class="btn btn-success" type="submit" name="" value="Crear +">
+							<input class="btn btn-success" type="submit" name="" value="Crear Encuesta">
 						</div><br><br>
-					
 				</div>
 			<?= form_close()?>
 		</div>
-
-</body>
-
+	</body>
 </html>
--- CodeIgniter/application/views/preguntas/create.php
+++ CodeIgniter/application/views/preguntas/create.php
@@ -1,42 +1,38 @@
 <?php
-			// Atributos de la caja de texto
-			$pregunta = array(
-				'name' => 'pregunta',
-				'placeholder' => '',
-				'required' => 'required'
-			);
-			
-			// url para preguntasController/crearPregunta Espera
-			//$idCuestionario = $this->uri->segment(4);
-			$crearPreguntas_url = base_url() . "Preguntas/create";
-		?>
-
-		<div class="container">
-				<div class="form-group">
-					<!-- label, input -->
-					<br>
-				</div>
-				<div class="form-group">
-				</div>
+		// Atributos de la caja de texto
+		$pregunta = array(
+			'name' => 'pregunta',
+			'placeholder' => '',
+			'required' => 'required'
+		);
+		
+		$crearPreguntas_url = base_url() . "Preguntas/create";
+?>
+	<div class="container">
+		<div class="form-group">
+			<!-- label, input --><br>
 		</div>
-		 <div class="container">
-		 	<?= form_open($crearPreguntas_url) ?>
-                <div class="card indigo form-white">
-                    <div class="card-body">
-                        <h2 class="text-center white-text py-3"><i class="fa fa-user"></i> Crear pregunta</h2>
-                        <!--Body-->
-                        <div class="">
-                            <i class="fa fa-envelope prefix white-text"></i>
-                            <label><h5>Escribe tu pregunta :</h5></label>
-                            <input maxlength="100" type="search" id="pregunta" name="pregunta" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"required autocomplete="off">  
-                        </div>
-                        <br><br>
-                        <div class="text-center">
-                            <a href="<?php echo $crearPreguntas_url?>">
-								<input class="btn btn-success" type="submit" name="" value="Crear Pregunta">
-							</a>
-                        </div>
+		<div class="form-group">
+		</div>
+	</div>
+	<div class="container">
+	 	<?= form_open($crearPreguntas_url) ?>
+            <div class="card indigo form-white">
+                <div class="card-body">
+                    <h2 class="text-center white-text py-3"><i class="fa fa-user"></i>Crear pregunta</h2>
+                    <!--Body-->
+                    <div class="">
+                        <i class="fa fa-envelope prefix white-text"></i>
+                        <label><h5>Escribe tu pregunta: </h5></label>
+                        <input maxlength="100" type="search" id="pregunta" name="pregunta" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required autocomplete="off" placeholder="Escribe tu pregunta">  
                     </div>
-                </div>
-            <?= form_close()?>
-        </div>
+                    <br><br>
+                    <div class="text-center">
+                        <a href="<?php echo $crearPreguntas_url?>">
+							<input class="btn btn-success" type="submit" name="" value="Crear Pregunta">
+						</a>
+                    </div>
+                 </div>
+            </div>
+        <?= form_close()?>
+    </div>
